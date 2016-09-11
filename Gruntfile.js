391a2e147a516b71eea4dac4305a854fc3b03657
Anja Ishmukhametova <miukki@gmail.com>
2016-09-12 02:36:14 +0800

    fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,22 @@
-
-
 module.exports = function(grunt) {
+  'user strict';
+
+  //before task loader init new rule 
+   require('./rules/ngerror').init();
+
+    // Load htmlhint plugin. But actually you can also create your wrapper/ withour installation -- 
+    //please look the code 
+    //subl node_modules/grunt-htmlhint/tasks/htmlhint.js 
+    
+    /*
+        register task and call 
+        var HTMLHint = require("htmlhint").HTMLHint;
+
+        grunt.registerMultiTask('htmlhint', 'Validate html files with htmlhint.', function() {
+    */
+
+    grunt.loadNpmTasks('grunt-htmlhint');
+
     // Project configuration.
     grunt.initConfig({
         htmlhint: {
@@ -14,8 +30,6 @@ module.exports = function(grunt) {
     });
 
 
-    // Load htmlhint plugin.
-    grunt.loadNpmTasks('grunt-htmlhint');
 
     // Register htmlhint as a default task
     grunt.registerTask('default', ['htmlhint']);
--- rules/ngerror.js
+++ rules/ngerror.js
@@ -1,13 +1,24 @@
-HTMLHint.addRule({
-    id: 'ngerror',
-    description: 'finds and reports all attributes of type `ngerror` with non-even numbers of single quotes.',
-    init: function(parser, reporter){
-        var self = this;
-        parser.addListener('tagstart', function(event){
-            var mapAttrs = parser.getMapAttrs(event.attrs);
-            if ( mapAttrs.hasOwnProperty('ngerror') && ( mapAttrs.ngerror.match(/'/g).length % 2 ) !== 0 ) {
-                    reporter.info('You must close and open all occurences of single quotes', event.line, event.col, self, event.raw);
-            }    
-        });
-    }
-});
+var HTMLHint  = require('htmlhint').HTMLHint;
+
+
+module.exports = {
+    init: initRule 
+};
+
+
+function initRule () {
+    HTMLHint.addRule({
+        id: 'ngerror',
+        description: 'finds and reports all attributes of type `ngerror` with non-even numbers of single quotes.',
+        init: function(parser, reporter){
+            var self = this;
+            parser.addListener('tagstart', function(event){
+                var mapAttrs = parser.getMapAttrs(event.attrs);
+                if ( mapAttrs.hasOwnProperty('ngerror') && ( mapAttrs.ngerror.match(/'/g).length % 2 ) !== 0 ) {
+                        reporter.info('You must close and open all occurences of single quotes', event.line, event.col, self, event.raw);
+                }    
+            });
+        }
+    });    
+}
+
